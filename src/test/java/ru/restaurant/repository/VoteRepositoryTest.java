0fa617cdf98a0eca1b5bc45828d9fd67a7652e6d
ilya <gusevia7@gmail.com>
2021-01-12 15:12:05 +0300

    Add getByDate vote test and check mvn test

--- src/test/java/ru/restaurant/repository/VoteRepositoryTest.java
+++ src/test/java/ru/restaurant/repository/VoteRepositoryTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import ru.restaurant.model.Vote;
 import ru.restaurant.web.AbstractControllerTest;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -24,5 +25,12 @@ class VoteRepositoryTest extends AbstractControllerTest {
         List<Vote> expected= Stream.of(VOTE_2,VOTE_1).collect(Collectors.toList());
         TEST_MATCHER.assertMatch(actual,expected);
 
+    }
+
+    @Test
+    void getByDate() {
+        List<Vote> actual= voteRepository.getByDate(LocalDate.now());
+        List<Vote> expected= Stream.of(VOTE_2,VOTE_3).collect(Collectors.toList());
+        TEST_MATCHER.assertMatch(actual,expected);
     }
 }
